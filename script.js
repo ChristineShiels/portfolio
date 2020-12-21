b08fccfebc97476305e9667666c91a5fa0c75a26
christineshiels <christineshiels@gmail.com>
2020-12-20 23:35:21 -0800

    resposive complete :airplane: :rocket: :sparkles:

--- script.js
+++ script.js
@@ -30,8 +30,15 @@ grrlApp.projectSwitch = function() {
     $(".projectList a").on("click", function(e) {
         console.log(this);
         if ($(this).hasClass("project01")) {
-            console.log("project01");
-            $(".projectPortal").hide().html('<p class="projectText">My first jQuery project - a simple text adventure built in one week</p><img src="./assets/neonFramejQueryAdventure.jpg" alt="choose your own adventure style web app"><a class="project" href="https://christineshiels.github.io/">click to view project</a> ').slideUp(700).slideDown(700);
+            $(".projectPortal").hide().html('<p class="projectText">My first jQuery project - a simple text adventure built in one week</p><img src="./assets/neonFramejQueryAdventure.jpg" alt="choose your own adventure style web app"><a class="project" href="https://christineshiels.github.io/" target="_blank">click to view project</a> ').slideUp(700).slideDown(700);
+        }else if ($(this).hasClass("project02")) {
+            $(".projectPortal").hide().html('<p class="projectText">My first React project - an app to record and review craft beer tasting notes, built in one week</p><img src="./assets/neonFrameTapp.jpg" alt="beer tasting web app"><a class="project" href="https://christineshiels.github.io/project05/" target="_blank">click to view project</a> ').slideUp(700).slideDown(700);
+        }else if ($(this).hasClass("project03")) {
+            $(".projectPortal").hide().html('<p class="projectText">Group React project - a mock client request to allow user to choose a color from a makeupp API and return an artwork containing that color from a museum API</p><img src="./assets/neonFrameHue.jpg" alt="API project web app"><a class="project" href="https://hue-are-you.github.io/hue-are-you/" target="_blank">click to view project</a> ').slideUp(700).slideDown(700);
+        }else if ($(this).hasClass("project04")) {
+            $(".projectPortal").hide().html('<p class="projectText">Group API project - my first API app uses MapQuest and Leafly APIs to find restaurants near to user provided location and offer directions</p><img src="./assets/neonFrameMaps.jpg" alt="Food Finder web app"><a class="project" href="https://christinebenproj4org.github.io/christineBenProj4/" target="_blank">click to view project</a> ').slideUp(700).slideDown(700);
+        }else if ($(this).hasClass("project05")) {
+            $(".projectPortal").hide().html('<p class="projectText">My first JavaScript project - a word-guessing game inspired by the game show, built in one week</p><img src="./assets/neonFrameLingo.jpg" alt="word game web app"><a class="project" href="http://lazrgrrl.com/lingo.html" target="_blank">click to view project</a> ').slideUp(700).slideDown(700);
         }
     })
 }
